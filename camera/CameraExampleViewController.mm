041035f4956b5959ecd9cfa177137ab4a00d24f2
Nivek Gnourt <nivekgnourt@gmail.com>
2017-07-21 20:18:00 -0700

    test

--- camera/CameraExampleViewController.mm
+++ camera/CameraExampleViewController.mm
@@ -30,18 +30,18 @@ static NSString* model_file_type = @"pb";
 // file created by the convert_graphdef_memmapped_format utility that wraps a
 // GraphDef and parameter file that can be mapped into memory from file to
 // reduce overall memory usage.
-const bool model_uses_memory_mapping = false;
+const bool model_uses_memory_mapping = true;
 // If you have your own model, point this to the labels file.
 static NSString* labels_file_name = @"retrained_labels";
 static NSString* labels_file_type = @"txt";
 // These dimensions need to match those the model was trained with.
-const int wanted_input_width = 224;
-const int wanted_input_height = 224;
+const int wanted_input_width = 299;
+const int wanted_input_height = 299;
 const int wanted_input_channels = 3;
-const float input_mean = 117.0f;
-const float input_std = 1.0f;
-const std::string input_layer_name = "input";
-const std::string output_layer_name = "softmax1";
+const float input_mean = 128.0f;
+const float input_std = 128.0f;
+const std::string input_layer_name = "Mul";
+const std::string output_layer_name = "final_result";
 
 static void *AVCaptureStillImageIsCapturingStillImageContext =
     &AVCaptureStillImageIsCapturingStillImageContext;
